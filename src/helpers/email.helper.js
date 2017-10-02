13c52d439ac15d93b49fd5c086bb6bb6de4c0ac8
mewild <michael@wildapplications.com>
2017-10-02 11:54:06 +0100

    Removed protected route

--- src/helpers/email.helper.js
+++ src/helpers/email.helper.js
@@ -12,21 +12,16 @@ transporter = nodemailer.createTransport({
 var emailer = {};
 
 emailer.send = function(call, callback){
-  jwt.verify(call.metadata.get('authorization')[0], process.env.JWT_SECRET, function(err, token){
-    if(err){
-      return callback({message:err},null);
+  var options = {
+    to: call.request.recipient,
+    subject: call.request.subject,
+    text: call.request.content
+  };
+  transporter.sendMail(options, function(error, response){
+    if(error){
+      return callback({message: JSON.stringify(error)}, null);
     }
-    var options = {
-      to: call.request.recipient,
-      subject: call.request.subject,
-      text: call.request.content
-    };
-    transporter.sendMail(options, function(error, response){
-      if(error){
-        return callback({message: JSON.stringify(error)}, null);
-      }
-      callback(null, {result: true});
-    });
+    callback(null, {result: true});
   });
 }
 
